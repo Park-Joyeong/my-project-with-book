b27bdc0527c170c4e653b121e73cae6e172127cd
Park Joyeong <drhamst@gmail.com>
2021-05-13 22:57:55 +0900

    Doing chapter 3.

--- src/main/java/com/joyeong/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/joyeong/book/springboot/service/posts/PostsService.java
@@ -42,4 +42,12 @@ public class PostsService {
     public List<PostsListResponseDto> findAllDesc() {
         return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
     }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("해당 게시글이 업습니다. id=" + id)
+        );
+        postsRepository.delete(posts);
+    }
 }
